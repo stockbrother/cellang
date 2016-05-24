f270e9fb14f7771a7c663c69abfd04bc0551b9e9
wuzhen <zhen.wu@hpe.com>
2016-05-24 18:15:23 +0800

    a

--- cellang-webclient/src/main/java/org/cellang/webclient/gwt/client/CellangEntryPoint.java
+++ cellang-webclient/src/main/java/org/cellang/webclient/gwt/client/CellangEntryPoint.java
@@ -2,11 +2,11 @@ package org.cellang.webclient.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 
-public class CellangEntrypoint implements EntryPoint {
+public class CellangEntryPoint implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-
+		System.out.println("onModuleLoad");
 	}
 
 }
