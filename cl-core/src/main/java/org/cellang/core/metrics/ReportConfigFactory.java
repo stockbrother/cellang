221b90bca8aef6d26f738341c199219485f55f2e
wuzhen <zhen.wu@hpe.com>
2016-09-01 14:43:37 +0800

    a

--- cl-core/src/main/java/org/cellang/core/metrics/ReportConfigFactory.java
+++ cl-core/src/main/java/org/cellang/core/metrics/ReportConfigFactory.java
@@ -2,6 +2,8 @@ package org.cellang.core.metrics;
 
 import org.cellang.core.entity.BalanceSheetItemEntity;
 import org.cellang.core.entity.BalanceSheetReportEntity;
+import org.cellang.core.entity.CashFlowStatementItemEntity;
+import org.cellang.core.entity.CashFlowStatementReportEntity;
 import org.cellang.core.entity.CustomizedItemEntity;
 import org.cellang.core.entity.CustomizedReportEntity;
 import org.cellang.core.entity.EntityConfigFactory;
@@ -11,14 +13,19 @@ import org.cellang.core.entity.IncomeStatementReportEntity;
 public class ReportConfigFactory {
 	public ReportConfig balanceSheetReportConfig;
 	public ReportConfig incomeStatementReportConfig;
-	public ReportConfig customizedReportConfig;	
+	public ReportConfig customizedReportConfig;
 	public ReportConfig cashFlowStatementReportConfig;
 
 	public ReportConfigFactory(EntityConfigFactory ecf) {
 		this.balanceSheetReportConfig = new ReportConfig(ecf.get(BalanceSheetReportEntity.class),
 				ecf.get(BalanceSheetItemEntity.class));
+
 		this.incomeStatementReportConfig = new ReportConfig(ecf.get(IncomeStatementReportEntity.class),
 				ecf.get(IncomeStatementItemEntity.class));
+
+		this.cashFlowStatementReportConfig = new ReportConfig(ecf.get(CashFlowStatementReportEntity.class),
+				ecf.get(CashFlowStatementItemEntity.class));
+
 		this.customizedReportConfig = new ReportConfig(ecf.get(CustomizedReportEntity.class),
 				ecf.get(CustomizedItemEntity.class));
 
